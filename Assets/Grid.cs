564c4cfd13d712417d545f5012d62ff1f1a18f5b
Brago <bragolab@gmail.com>
2017-01-21 02:44:16 +0100

    updated gamecontroller

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -9,19 +9,27 @@ public class Grid : MonoBehaviour {
     public int YOffset = 10;
 
     public Transform WaveContainerPrefab;
-    public List<Wave> Waves;
 
     private void Start() {
         SetWaves();
     }
 
     public void SetWaves() {
-        for (int i = 0; i < Waves.Count; i++) {
-            Transform newTransform =  Instantiate<Transform>(WaveContainerPrefab);
+        // Target wave
+        Transform newTransformTarget = Instantiate<Transform>(WaveContainerPrefab);
+        newTransformTarget.localScale = new Vector3(Width, Height, 1);
+        newTransformTarget.position = new Vector3(0, Height, 0);
+
+        Wave newWaveTarget = Instantiate<Wave>(GameController.Instance.targetWave);
+        newWaveTarget.transform.position = newTransformTarget.position + new Vector3((-Width / 2) + 0.5f, 0, -1);
+
+        // 
+        for (int i = 0; i < GameController.Instance.gameWaves.Count; i++) {
+            Transform newTransform = Instantiate<Transform>(WaveContainerPrefab);
             newTransform.localScale = new Vector3(Width, Height, 1);
             newTransform.position = new Vector3(0, (i * Height) + (i * YOffset), 0);
 
-            Wave newWave = Instantiate<Wave>(Waves[i]);
+            Wave newWave = Instantiate<Wave>(GameController.Instance.gameWaves[i]);
             // newWave.transform.position = newTransform.position + new Vector3(-Width / 2, -Height/2, -1);
             newWave.transform.position = newTransform.position + new Vector3((-Width / 2) + 0.5f, 0, -1);
         }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public Wave targetWave;
-    public Wave gameWave;
 
-	void Start ()
+    public Wave targetWave;
+    public List<Wave> gameWaves;
+
+    public static GameController Instance;
+
+    void Start ()
     {
-		
+        if (Instance == null)
+            Instance = this;
 	}
 	
 	void Update ()
     {
 		if (Input.GetKeyDown(KeyCode.Space))
         {
-            targetWave.SumWave(gameWave);
+            targetWave.SumWave(gameWaves[0]);
 
             if (targetWave.IsWaveZero())
             {
