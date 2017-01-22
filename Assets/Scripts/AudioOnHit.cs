865ad7ec3a6bd178763a9d7cf6c95caac09ec00c
Michele <cat.piro@gmail.com>
2017-01-22 17:41:28 +0100

    Added SFX

---
+++ Assets/Scripts/AudioOnHit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnHit : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip[] clips;
+    public AudioClip rareClip;
+
+    void OnMouseUpAsButton()
+    {
+        source.clip = clips[Random.Range(0, clips.Length)];
+        if (Random.value < 0.05f) source.clip = rareClip;
+        source.Play();
+    }
+}
