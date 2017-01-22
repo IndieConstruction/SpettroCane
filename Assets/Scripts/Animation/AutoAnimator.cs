8c949dbb131e8b8f7057a44899be03a11afcc276
Michele <cat.piro@gmail.com>
2017-01-22 15:10:39 +0100

    Updated scene. added auto animator

---
+++ Assets/Scripts/Animation/AutoAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class AutoAnimator : MonoBehaviour
+{
+    public Animator animator;
+
+    public int MinAppearTime = 10;
+    public int MaxAppearTime = 15;
+
+    public int MinReappearTime = 5;
+    public int MaxReappearTime = 7;
+
+    void Awake ()
+    {
+        animator.Stop();
+	}
+
+    IEnumerator AppearCO()
+    {
+        animator.Stop();
+        yield return new WaitForSeconds(Random.Range(MinAppearTime, MaxAppearTime));
+        //animator.Play();
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(MinReappearTime, MaxReappearTime));
+            // animator.Play();
+        }
+    }
+
+}
